ca7504a3e5bba1f82c500967c1ef93afe23701a1
Mike Jin <service@amdiscountfurniture.com>
2022-03-29 10:32:18 -0400

    new

--- models/Activity.js
+++ models/Activity.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const ActivitySchema = new mongoose.Schema(
 	{
-		title: { type:String, required: true },
-		sku:{ type: String, required: true, unique: true },
+		QRID: { type:String, required: true, unique: true },
+		sku:{ type: String, required: true },
 		desc:{ type: String, required: false },
 		status: { type: String, required: true },
 		price: { type: Number, required: false },
