c0f7e0c8c6c98f389553ae474751fb5e0aeda502
Mike Jin <service@amdiscountfurniture.com>
2022-03-29 11:25:51 -0400

    new

--- models/Activity.js
+++ models/Activity.js
@@ -5,7 +5,7 @@ const ActivitySchema = new mongoose.Schema(
 		QRID: { type:String, required: true, unique: true },
 		sku:{ type: String, required: true },
 		desc:{ type: String, required: false },
-		status: { type: String, required: true },
+		status: { type: String, required: true }, //Code Generated, Stocked In, Stocked Out
 		price: { type: Number, required: false },
 		cost: { type: Number, required: false },
 		quantity: { type: Number, default: 0 }
--- models/Product.js
+++ models/Product.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const ProductSchema = new mongoose.Schema(
 	{
 		title: { type:String, required: true },
-		sku:{ type: String, required: true, unique: true },
+		productID:{ type: String, required: true, unique: true }, //sku
 		desc:{ type: String, required: false },
 		img: { type: String, required: false },
 		category: { type: Array, required: false },
